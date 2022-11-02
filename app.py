2e82d20671c528284f4e5107487bd0e61c95e262
jeremyKr <wjddlr905@gmail.com>
2022-11-02 16:44:22 +0900

    Update POST

--- app.py
+++ app.py
@@ -17,42 +17,85 @@ db = client.db
 def home():
     return render_template('index.html')
 
-@app.route("/hana/post", methods=["POST"])
-def hana_post():
-    name_receive = request.form['name_give']
-    comment_receive = request.form['comment_give']
-
-    doc = {
-        'name': name_receive,
-        'comment': comment_receive
-    }
-    db.homework.insert_one(doc)
-
-    return jsonify({'msg':'저장 완료!'})
-
-
 @app.route('/hana')
 def hana():
     return render_template('Hana.html')
 
+@app.route("/hana/post", methods=["POST"])
+def hana_post():
+    name = request.form['name']
+    text = request.form['text']
+    doc = {
+        'name': name,
+        'text': text
+    }
+    db.commentsHana.insert_one(doc)
+    return jsonify({'msg':'저장 완료!'})
+
 @app.route("/hana/get", methods=["GET"])
 def hana_get():
-    comment_list = list(db.comments.find({}, {'_id': False}))
+    comment_list = list(db.commentsHana.find({}, {'_id': False}))
     return jsonify({'comments': comment_list})
 
 @app.route('/jeongik')
 def jeongik():
     return render_template('Jeongik.html')
 
+@app.route("/jeongik/post", methods=["POST"])
+def jeongik_post():
+    name = request.form['name']
+    text = request.form['text']
+    doc = {
+        'name': name,
+        'text': text
+    }
+    db.commentsJeongik.insert_one(doc)
+    return jsonify({'msg':'저장 완료!'})
+
+@app.route("/jeongik/get", methods=["GET"])
+def jeongik_get():
+    comment_list = list(db.commentsJeongik.find({}, {'_id': False}))
+    return jsonify({'comments': comment_list})
 
 @app.route('/sanghyun')
 def sanghyun():
     return render_template('Sanghyun.html')
 
+@app.route("/sanghyun/post", methods=["POST"])
+def sanghyun_post():
+    name = request.form['name']
+    text = request.form['text']
+    doc = {
+        'name': name,
+        'text': text
+    }
+    db.commentsSanghyun.insert_one(doc)
+    return jsonify({'msg':'저장 완료!'})
+
+@app.route("/sanghyun/get", methods=["GET"])
+def sanghyun_get():
+    comment_list = list(db.commentsSanghyun.find({}, {'_id': False}))
+    return jsonify({'comments': comment_list})
 
 @app.route('/yujin')
 def yujun():
     return render_template('Yujin.html')
+
+@app.route("/yujin/post", methods=["POST"])
+def yujin_post():
+    name = request.form['name']
+    text = request.form['text']
+    doc = {
+        'name': name,
+        'text': text
+    }
+    db.commentsYujin.insert_one(doc)
+    return jsonify({'msg':'저장 완료!'})
+
+@app.route("/yujin/get", methods=["GET"])
+def yujin_get():
+    comment_list = list(db.commentsYujin.find({}, {'_id': False}))
+    return jsonify({'comments': comment_list})
 
 
 if __name__ == '__main__':
